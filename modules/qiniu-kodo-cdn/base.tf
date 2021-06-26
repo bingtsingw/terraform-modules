68e66b226a25323bea8c11160e2cd34131a25735
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2021-06-26 14:50:54 +0800

    qiniu-kodo-cdn add dns and health check

--- modules/qiniu-kodo-cdn/base.tf
+++ modules/qiniu-kodo-cdn/base.tf
@@ -3,5 +3,13 @@ terraform {
     qiniu = {
       source = "bingtsingw/qiniu"
     }
+
+    alicloud = {
+      source = "aliyun/alicloud"
+    }
+
+    updown = {
+      source = "mvisonneau/updown"
+    }
   }
 }
--- modules/qiniu-kodo-cdn/main.tf
+++ modules/qiniu-kodo-cdn/main.tf
@@ -27,3 +27,16 @@ resource "qiniu_cdn_domain" "domain" {
     }
   }
 }
+
+resource "alicloud_dns_record" "dns" {
+  name        = "${element(split(".", var.domain), 1)}.${element(split(".", var.domain), 2)}"
+  host_record = element(split(".", var.domain), 0)
+  type        = "CNAME"
+  value       = qiniu_cdn_domain.domain.cname
+}
+
+module "health-check" {
+  source = "github.com/bingtsingw/terraform-modules.git//modules/updown-check?ref=v0.5.0"
+
+  url = "${qiniu_cdn_domain.domain.protocol}://${alicloud_dns_record.dns.host_record}.${alicloud_dns_record.dns.name}/${tolist(qiniu_cdn_domain.domain.source)[0].test_url_path}"
+}
